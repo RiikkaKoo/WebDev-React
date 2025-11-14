882566a651711a785714a4725db5206380a6b60c
riikka <riikaka@metropolia.fi>
2025-11-14 15:10:08 +0200

    Hooks: Task 1 done

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,47 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Counter from "../components/Counter";
 import MediaRow from "../components/MediaRow";
 import SingleView from "../components/SingleView";
 
-const mediaArray = [
-  {
-    media_id: 8,
-    user_id: 5,
-    filename: "https://place-hold.it/1200x800.jpg&text=Pic1&fontsize=120",
-    thumbnail: "http://place-hold.it/320/240.jpg&text=Thumb2&fontsize=20",
-    filesize: 170469,
-    media_type: "image/jpeg",
-    title: "Picture 1",
-    description: "This is a placeholder picture.",
-    created_at: "2024-01-07T20:49:34.000Z",
-  },
-  {
-    media_id: 9,
-    user_id: 7,
-    filename: "https://place-hold.it/800x600.jpg&text=Pic2&fontsize=72",
-    thumbnail: "http://place-hold.it/320/240.jpg&text=Thumb3&fontsize=20",
-    filesize: 1002912,
-    media_type: "image/jpeg",
-    title: "Pic 2",
-    description: "",
-    created_at: "2024-01-07T21:32:27.000Z",
-  },
-  {
-    media_id: 17,
-    user_id: 2,
-    filename: "https://www.w3schools.com/html/mov_bbb.mp4",
-    thumbnail: "http://place-hold.it/320/240.jpg&text=Thumb1&fontsize=20",
-    filesize: 1236616,
-    media_type: "video/mp4",
-    title: "Bunny",
-    description: "Butterflies fly around the bunny.",
-    created_at: "2024-01-07T20:48:13.000Z",
-  },
-];
+const fetchData = async (url, options = {}) => {
+  // console.log('fetching data from url: ', url);
+  const response = await fetch(url, options);
+  const json = await response.json();
+  if (!response.ok) {
+    // console.log('json', json);
+    if (json.message) {
+      throw new Error(json.message);
+    }
+    throw new Error(`Error ${response.status} occured`);
+  }
+  return json;
+};
 
 const Home = () => {
   const [selectedItem, setSelectedItem] = useState(null);
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const getMedia = async () => {
+    const data = await fetchData("./data.json");
+    setMediaArray(data);
+  };
+
+  useEffect(() => {
+    getMedia();
+  }, []);
 
   return (
     <>
