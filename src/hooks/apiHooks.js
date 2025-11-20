9f87d6c24ab3c60acbe5a93b2ba1a1e43a99930c
riikka <riikaka@metropolia.fi>
2025-11-20 13:23:09 +0200

    Updated the apiHooks code based on the teacher example.

--- src/hooks/apiHooks.js
+++ src/hooks/apiHooks.js
@@ -7,26 +7,25 @@ const AUTH_API = import.meta.env.VITE_AUTH_API + "/users/";
 const useMedia = () => {
   const [mediaArray, setMediaArray] = useState([]);
 
-  try {
-    const getMedia = async () => {
-      const media = await fetchData(MEDIA_API);
+  useEffect(() => {
+    try {
+      const getMedia = async () => {
+        const mediaData = await fetchData(MEDIA_API);
 
-      const newArray = await Promise.all(
-        media.map(async (item) => {
-          const user = await fetchData(AUTH_API + item.user_id);
-          return { ...item, username: user.username };
-        })
-      );
-
-      setMediaArray(newArray);
-    };
-
-    useEffect(() => {
+        const newArray = await Promise.all(
+          mediaData.map(async (item) => {
+            const user = await fetchData(AUTH_API + item.user_id);
+            return { ...item, username: user.username };
+          })
+        );
+        // console.log(newArray);
+        setMediaArray(newArray);
+      };
       getMedia();
-    }, []);
-  } catch (error) {
-    console.log(error);
-  }
+    } catch (error) {
+      console.log("ERROR", error);
+    }
+  }, []);
   return { mediaArray };
 };
 
