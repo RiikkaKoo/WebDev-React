9c40d2331c0ea81ccd026043d0ea060fde50998c
riikka <riikaka@metropolia.fi>
2025-11-20 12:55:50 +0200

    Custom hooks pohjustus

---
+++ src/hooks/apiHooks.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import fetchData from "../utils/fetchData";
+
+const MEDIA_API = import.meta.env.VITE_MEDIA_API + "/media";
+const AUTH_API = import.meta.env.VITE_AUTH_API + "/users/";
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  useEffect(() => {
+    try {
+      const getMedia = async () => {
+        const media = await fetchData(MEDIA_API);
+
+        const newArray = await Promise.all(
+          media.map(async (item) => {
+            const user = await fetchData(AUTH_API + item.user_id);
+            return { ...item, username: user.username };
+          })
+        );
+
+        setMediaArray(newArray);
+      };
+      getMedia();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+};
+
+export { useMedia };
--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,36 +1,13 @@
-import { useEffect, useState } from "react";
-import fetchData from "../utils/fetchData.js";
+import { useState } from "react";
+import { useMedia } from "../hooks/apiHooks";
 
 import MediaRow from "../components/MediaRow";
 import SingleView from "../components/SingleView";
 
 const Home = () => {
   const [selectedItem, setSelectedItem] = useState(null);
-  const [mediaArray, setMediaArray] = useState([]);
 
-  const MEDIA_API = import.meta.env.VITE_MEDIA_API + "/media";
-  const AUTH_API = import.meta.env.VITE_AUTH_API + "/users/";
-
-  try {
-    const getMedia = async () => {
-      const media = await fetchData(MEDIA_API);
-
-      const newArray = await Promise.all(
-        media.map(async (item) => {
-          const user = await fetchData(AUTH_API + item.user_id);
-          return { ...item, username: user.username };
-        })
-      );
-
-      setMediaArray(newArray);
-    };
-
-    useEffect(() => {
-      getMedia();
-    }, []);
-  } catch (error) {
-    console.log(error);
-  }
+  const { mediaArray } = useMedia();
 
   return (
     <>
