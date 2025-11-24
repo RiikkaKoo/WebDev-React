b471251e1cd296ee5065cfc6e928d687211cf997
riikka <riikaka@metropolia.fi>
2025-11-24 17:33:48 +0200

    Test fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/~riikaka/OHTU_WebDevelopmentTasks/React/context",
+  base: "/~riikaka/OHTU_WebDevelopmentTasks/React/context/",
 });
